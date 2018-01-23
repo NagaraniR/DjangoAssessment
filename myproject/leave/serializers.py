2377c13866ccec77d982b81847d3f15011a193d5
Nagarani.R <nagaraniraja9677@gmail.com>
2018-01-23 13:00:34 +0530

    getting values from serializers for UI

--- myproject/leave/serializers.py
+++ myproject/leave/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from models import User
+from models import User, LeaveType
 
 
-class UserLeave(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
-		fields = '__all__'
+		fields = ('name', 'reporting_senior')
+
+class LeaveSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = LeaveType
+		fields = ('catagory',)
--- myproject/leave/views.py
+++ myproject/leave/views.py
@@ -5,14 +5,16 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.views import View
 from models import Designation, User, Status, LeaveType, LeaveCredit, LeaveRequest
-from serializers import UserLeave
+from serializers import UserSerializer, LeaveSerializer
 from django.http import HttpResponse, JsonResponse
 from django.views import generic
 
 def apply(request, employee_name):
-	queryset = User.objects.filter(name = employee_name)
-	serializer = UserLeave(queryset, many=True)
-	return JsonResponse(serializer.data, safe=False)
+	queryset_one = User.objects.filter(name = employee_name)
+	user_serializer = UserSerializer(queryset_one, many=True)
+	queryset_two = LeaveType.objects.all()
+	leave_serializer = LeaveSerializer(queryset_two, many=True)
+	return JsonResponse(leave_serializer.data, safe=False)
 	
 
 	
