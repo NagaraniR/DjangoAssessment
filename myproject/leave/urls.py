9226324bc8fec83bc53a7da7cca9ede42b2fc690
Nagarani.R <nagaraniraja9677@gmail.com>
2018-02-15 10:49:48 +0530

    validations for days and create url for pending records

--- myproject/leave/urls.py
+++ myproject/leave/urls.py
@@ -10,6 +10,7 @@ url(r'^user/apply/(?P<pk>\d+)/$', views.ApplyGetView.as_view(), name= 'user'),
 url(r'^apply/$', views.ApplyPostView.as_view(), name = 'apply'),
 url(r'^user/history/(?P<pk>\d+)/$', views.UserHistoryView.as_view(), name = 'history'),
 url(r'^WAPPR/(?P<pk>\d+)/$', views.WAPPRView.as_view(), name = 'waiting for approval'),
+url(r'^user/pending/records/(?P<pk>\d+)/$', views.PendingRecordView.as_view(), name= 'user'),
 
 	url(r'^deny/$', views.DenyView.as_view(), name='leave_deny'),
 	url(r'^approve/$', views.ApproveView.as_view(), name='leave_approve'),
